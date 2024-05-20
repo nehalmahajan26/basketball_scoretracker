d85929a1965bccd99703bf935f1f2bc672b3def7
Nehal Mahajan <nehalmahajan28@gmail.com>
2024-05-20 20:05:59 +0530

    Files added
    a game score tracker

---
+++ index.js
@@ -0,0 +1,9 @@
+function home1(){
+    document.getElementById("res-home").textContent=parseInt(document.getElementById("res-home").textContent) + 1;
+}
+function home2(){
+    document.getElementById("res-home").textContent+=2;
+}
+function home2(){
+    document.getElementById("res-home").textContent +=3;
+}
